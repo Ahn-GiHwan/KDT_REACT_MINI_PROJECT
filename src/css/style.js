e807b1b184524c6641572af1121f4ef258497f7b
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-18 13:31:11 +0900

    style: styled-components를 이용하여 container, main style 작성

---
+++ src/css/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+`;
+
+export const Main = styled.main`
+  position: relative;
+  width: 80%;
+  margin: 0 auto;
+`;
