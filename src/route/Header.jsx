f00ace58e7a118c926cd665e922ab489a76a5ad1
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-23 19:46:19 +0900

    fix: change to .js to .jsx

---
+++ src/route/Header.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link, NavLink } from "react-router-dom";
+import { Nav, Navbar, NavbarBrand, NavItem } from "reactstrap";
+import { HeaderTag } from "../css/style";
+
+const Header = () => {
+  const { userInfo } = useSelector((state) => state.user);
+  return (
+    <HeaderTag>
+      <Navbar>
+        <NavbarBrand>
+          <Link to={"/"}>
+            <img
+              src={require("../img/layout/logo.png").default}
+              height="90px"
+              width="200px"
+              alt=""
+            />
+          </Link>
+        </NavbarBrand>
+        {/* <NavbarToggler /> */}
+        <Nav className="mr-auto menu">
+          {userInfo?.user_email || (
+            <NavItem>
+              <NavLink to={"/login"}>로그인</NavLink>
+            </NavItem>
+          )}
+          <NavItem>
+            <NavLink to={"/board"}>게시판</NavLink>
+          </NavItem>
+          {userInfo?.user_email || (
+            <NavItem>
+              <NavLink to={"/register"}>사용자 등록</NavLink>
+            </NavItem>
+          )}
+          <NavItem>
+            <NavLink to={"/product"}>구매 하기</NavLink>
+          </NavItem>
+          {userInfo?.user_email && (
+            <>
+              <NavItem>
+                <NavLink to={"/naverApi"}>상품 등록</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to={"/cart"}>장바구니</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to={"/history"}>구매 내역</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to={"/"}>로그아웃</NavLink>
+              </NavItem>
+            </>
+          )}
+        </Nav>
+      </Navbar>
+    </HeaderTag>
+  );
+};
+
+export default Header;
