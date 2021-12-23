e4615a9cf70f71a796e9b76c40e543476b3b627d
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-23 18:15:31 +0900

    feat: add component
    CreateBoard: 게시글을 추가하는 컴포넌트
    
    ScrollToTop: 페이지 이동 시 스크롤 최상단으로 올려주는 컴포넌트

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -6,6 +6,7 @@ import "../css/style.css";
 import "../css/toy.css";
 
 import Board from "./board/Board";
+import CreateBoard from "./board/CreateBoard";
 import Login from "./login/Login";
 import Register from "./register/Register";
 import Product from "./product/Product";
@@ -13,18 +14,21 @@ import Cart from "./cart/Cart";
 import History from "./history/History";
 import Naver from "./naver/Naver";
 import { useState } from "react";
+import ScrollToTop from "./common/ScrollToTop";
 
 function App() {
   const [userId, setUserId] = useState("");
 
   return (
     <div className="App">
+      <ScrollToTop />
       <Header />
       <Route exact path="/" component={Login} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/naverApi" component={Naver} />
       <Route exact path="/board" component={Board} />
+      <Route exact path="/board/create" component={CreateBoard} />
       <Route path="/product" render={() => <Product userId={userId} />} />
       <Route path="/cart" render={() => <Cart userId={userId} />} />
       <Route path="/history" render={() => <History userId={userId} />} />
