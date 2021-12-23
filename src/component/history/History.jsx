f4c8c043b37f92202901ea417c87c5d9ba8f654c
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-23 18:25:54 +0900

    feat: 구매내역에 관한 컴포넌트
    구매내역에 대한 차트 추가

--- src/component/history/History.jsx
+++ src/component/history/History.jsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { fetchGetBuyCategoryList, fetchGetBuyList } from "../../utils/buyFetch";
+import InfoTable from "../common/InfoTable";
+import Chart from "./Chart";
 
-const History = (props) => {
+const History = () => {
+  const [chartData, setChartData] = useState([]);
+  const getBuyList = async () => {
+    try {
+      const data = await fetchGetBuyList();
+      console.log(data);
+    } catch (error) {
+      console.dir(error);
+    }
+  };
+
+  const getCategoryInfo = async () => {
+    try {
+      const { json } = await fetchGetBuyCategoryList();
+      setChartData(json);
+    } catch (error) {
+      console.dir(error);
+    }
+  };
+  useEffect(() => {
+    getBuyList();
+    getCategoryInfo();
+  }, []);
+
   return (
     <div>
       <h2>사용자 구매내역</h2>
+      <div style={{ width: "200px", height: "300px" }}>
+        <Chart name="category2" value="amount" data={chartData} />
+      </div>
+      <InfoTable />
     </div>
   );
 };
