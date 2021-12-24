ec53df4f7e119df779745c2c70c965680e2eabcc
Ahn-Gihwan <ahngh0113@naver.com>
2021-12-24 13:49:35 +0900

    feat: user 정보를 불러오는 함수

--- src/utils/userFetch.js
+++ src/utils/userFetch.js
@@ -1,9 +1,30 @@
 import axios from "axios";
 
 export const fetchLogin = async (user_email, user_password) => {
-  const { data } = await axios.post("api/cart?type=order", {
+  const { data } = await axios.post("api/user?type=login", {
     user_email,
     user_password,
+  });
+
+  return data;
+};
+
+export const fetchSessionLogin = async (user_email, user_password) => {
+  console.log("fetchSessionLogin");
+  console.log(user_email);
+  console.log(user_password);
+  const { data } = await axios.post("api/user?type=sessionSignin", {
+    user_email,
+    user_password,
+  });
+
+  return data;
+};
+
+export const fetchSessionCheck = async (token_id, token_name) => {
+  const { data } = await axios.post("api/user?type=sessionCheck", {
+    token_id,
+    token_name,
   });
 
   return data;
